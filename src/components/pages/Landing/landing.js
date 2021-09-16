ad60dfeb77d20dc3cb848e77b0e6c752d57e3aa7
Shayanna Gatchalian <36995948+Moomuz@users.noreply.github.com>
2021-09-16 09:00:12 -0700

    Update landing.js

--- src/components/pages/Landing/landing.js
+++ src/components/pages/Landing/landing.js
@@ -4,6 +4,9 @@ import './landing.css'
 import Navbar from '../../Navbar'
 
 import landingImage from '../Landing/landingImage.svg'
+import step1Image from '../Landing/step1Image.svg'
+import step2Image from '../Landing/step2Image.svg'
+import step3Image from '../Landing/step3Image.svg'
 
 function Landing(){
     return(
@@ -19,6 +22,34 @@ function Landing(){
                 </div>
                 <img src={landingImage} />
             </div>
+
+            <div className="landingSect2">
+                <h2>How it works</h2>
+
+                <div className="threeSteps">
+                    <div className="threeStepsRow">
+                        <div className="threeStepsCell step1">
+                            <h3>Create</h3>
+                            <img style={{maxWidth: '80%', height: '200px'}} src={step1Image} />
+                            <p>Create your profile by adding interests, classes, and more</p>
+                        </div>
+
+                        <div className="threeStepsCell step2">
+                            <h3>Swipe</h3>
+                            <img style={{maxWidth: '80%', height: '200px'}} src={step2Image} />
+                            <p>Look through potential study buddies</p>
+                        </div>
+
+                        <div className="threeStepsCell step3">
+                            <h3>Study</h3>
+                            <img style={{maxWidth: '80%', height: '200px'}} src={step3Image} />
+                            <p>Connect with other students and chat with them</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
