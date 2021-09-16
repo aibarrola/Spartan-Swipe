f9d1072043da03d156fb3888053e1e9d9faed862
Shayanna Gatchalian <36995948+Moomuz@users.noreply.github.com>
2021-09-16 09:00:31 -0700

    Update landing.js

--- src/components/pages/Landing/landing.js
+++ src/components/pages/Landing/landing.js
@@ -1,57 +1,69 @@
-import {Link} from 'react-router-dom'
-
-import './landing.css'
-import Navbar from '../../Navbar'
-
-import landingImage from '../Landing/landingImage.svg'
-import step1Image from '../Landing/step1Image.svg'
-import step2Image from '../Landing/step2Image.svg'
-import step3Image from '../Landing/step3Image.svg'
-
-function Landing(){
-    return(
-        <div className="landing">
-            <Navbar />
-
-            <div className="landingSect1">
-                <div className="sect1left">
-                    <h1 className="heroTitle"> Find your  <br /> Study Buddy. </h1> 
-                    <Link to="/register">
-                        <button className="createAccountBtn"> Create Account </button>
-                    </Link>
-                </div>
-                <img src={landingImage} />
-            </div>
-
-            <div className="landingSect2">
-                <h2>How it works</h2>
-
-                <div className="threeSteps">
-                    <div className="threeStepsRow">
-                        <div className="threeStepsCell step1">
-                            <h3>Create</h3>
-                            <img style={{maxWidth: '80%', height: '200px'}} src={step1Image} />
-                            <p>Create your profile by adding interests, classes, and more</p>
-                        </div>
-
-                        <div className="threeStepsCell step2">
-                            <h3>Swipe</h3>
-                            <img style={{maxWidth: '80%', height: '200px'}} src={step2Image} />
-                            <p>Look through potential study buddies</p>
-                        </div>
-
-                        <div className="threeStepsCell step3">
-                            <h3>Study</h3>
-                            <img style={{maxWidth: '80%', height: '200px'}} src={step3Image} />
-                            <p>Connect with other students and chat with them</p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-    )
+.landing{
+    height: 100vh;
+    background-color: #ffffffff;
 }
 
-export default Landing;
+
+.heroTitle{
+    font-size: 4rem;
+    color: #0055A2;
+    text-align: center;
+}
+
+.landingSect1{
+    margin-top: 5%;
+    display: flex;
+    justify-content: space-around;
+}
+
+.sect1left{
+    display: flex;
+    flex-direction: column;
+}
+
+.landingSect2{
+    margin-top: 15%;
+    text-align: center;
+    /* background-color: green; */
+    padding-bottom: 5%;
+    float: center;
+}
+
+.threeSteps{
+    display: table;
+    /* background-color: red; */
+    margin: 0 auto;
+}
+
+.threeSteps .threeStepsRow{
+    display: table-row;
+}
+
+.threeSteps .threeStepsCell{
+    display: table-cell;
+    width: 33%;
+    height: 350px;
+    padding: 0px 15px;
+    margin: 2%;
+    /* background-color: red; */
+    text-align: center;
+    border: 2px #E5E5E5 solid;
+    border-radius: 15px;
+}
+
+.createAccountBtn{
+    background-color: #0055A2;
+    color: white;
+    border: none;
+    border-radius: 25px;
+    font-size: 1.5rem;
+    padding: 10px 20px;
+    align-self: center;
+    margin-left: 100px;
+    text-align: center;
+}
+
+.createAccountBtn:hover{
+    background-color: #1760a5;
+
+}
