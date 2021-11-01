1267fefbada0f1f2e9d549c261a1805620ff2a89
AnthonyNguyenT <anthonynguyenthanh@gmail.com>
2021-10-31 22:03:33 -0700

    created profile reducer and dashboard, and getting user's profile

---
+++ SpartanSwipe-Backend/client/src/actions/profile.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+
+import { GET_PROFILE, PROFILE_ERROR } from './types';
+
+// Get current users profile
+export const getCurrentProfile = () => async dispatch => {
+    try
+    {
+        const res = await axios.get('/api/profile/me');
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    }
+    catch(error)
+    {
+        dispatch({ 
+            type: PROFILE_ERROR,
+            payload: { message: error.response.statusText, status: error.response.status }
+         })
+    }
+}
