f20220f72b999c06b14bab20043cb3998491b6f5
Angelo <vino_1105@hotmail.com>
2021-11-23 07:02:35 -0800

    setup landing page css

--- SpartanSwipe-Backend/client/src/components/layout/Landing.js
+++ SpartanSwipe-Backend/client/src/components/layout/Landing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './Landing.css'
 
 const Landing = ({ isAuthenticated }) => {
     if(isAuthenticated)
@@ -10,17 +11,16 @@ const Landing = ({ isAuthenticated }) => {
     }
     return (
         <section className="landing">
-        <div className="dark-overlay">
-            <div className="landing-inner">
-            <h1 className="x-large">SpartanSwipe</h1>
-            <p className="lead">
-                Find your study buddy and find success!
-            </p>
-            <div className="buttons">
-                <Link to='/register' className="btn btn-primary">Register</Link>
+            <div className="landingSect1">
+                <div className="sect1Left">
+                    <h1 className="heroTitle"> Find your <br /> Study Buddy. </h1>
+               
+                    <Link to='/register' 
+                        className="createAccountBtn"> Create Account 
+                    </Link>
+                </div>
+
             </div>
-            </div>
-        </div>
         </section>
     )
 }
