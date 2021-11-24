dd480acfe0ba422e4bd1dd547655822512ed7411
Angelo <vino_1105@hotmail.com>
2021-11-23 20:29:10 -0800

    finished edit profile design

--- SpartanSwipe-Backend/client/src/components/profile-forms/EditProfile.js
+++ SpartanSwipe-Backend/client/src/components/profile-forms/EditProfile.js
@@ -3,6 +3,7 @@ import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createProfile, getCurrentProfile } from '../../actions/profile';
+import './CreateProfile.css'
 
 const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentProfile, history }) => {
     const [formData, setFormData] = useState({
@@ -51,80 +52,64 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentP
     }
 
     return (
-        <Fragment>
-            <h1 className="large text-primary">
-            Create Your Profile
-        </h1>
-        <p className="lead">
-            <i className="fas fa-user"></i> Let's get some information to make your profile stand out
-        </p>
-        <small>* = required field</small>
-        <form className="form" onSubmit={e => onSubmit(e)}>
-            <div className="form-group">
-            <select name="department" value={department} onChange={e => onChange(e)}>
-                <option value="0">* Department</option>
-                <option value="Athletics (Intercollegiate)">Athletics (Intercollegiate)</option>
-                <option value="Charles W. Davidson College of Engineering">Charles W. Davidson College of Engineering</option>
-                <option value="College of Graduate Studies">College of Graduate Studies</option>
-                <option value="College of Health and Human Sciences">College of Health and Human Sciences</option>
-                <option value="College of Humanities and the Arts">College of Humanities and the Arts</option>
-                <option value="College of Professional and Global Education">College of Professional and Global Education</option>
-                <option value="College of Science">College of Science</option>
-                <option value="College of Social Sciences">College of Social Sciences</option> 
-                <option value="Connie L. Lurie College of Education">Connie L. Lurie College of Education</option>
-                <option value="Lucas College and Graduate School of Business">Lucas College and Graduate School of Business</option>
-                <option value="Undeclared">Undeclared</option>
-                <option value="Undergraduate Education">Undergraduate Education</option>
-                <option value="Other">Other</option>
-            </select>
-            <small className="form-text">
-                What department is your major under?
-            </small>
-            </div>
-            
-            <div className="form-group">
-            <input type="text" placeholder="* Degree(s) & Concentration(s)" name="degrees" value={degrees} onChange={e => onChange(e)}/>
-            <small className="form-text"
-                >Please use comma separated values (eg.
-                B:Biology,A:Chemistry,C:Microbiology)
-            </small>
-            <small className="form-text">
-                <i>[A: Associate, B: Bachelor's, C: Concentration, D: Doctorate, M: Master's]</i>
-            </small>
-            </div>
+        <div className="createProfile">
+        <div className="createProfileBox">
+            <h1 className="createProfileTitle"> Edit your profile </h1>
+                <form className="createProfileForm" onSubmit={e => onSubmit(e)}>
+                    <div className="registerInputContainer">
+                        <p className="inputLabel"> Department </p>
+                        <select name="department" value={department} onChange={e => onChange(e)}>
+                            <option value="0">Select your department</option>
+                            <option value="Athletics (Intercollegiate)">Athletics (Intercollegiate)</option>
+                            <option value="Charles W. Davidson College of Engineering">Charles W. Davidson College of Engineering</option>
+                            <option value="College of Graduate Studies">College of Graduate Studies</option>
+                            <option value="College of Health and Human Sciences">College of Health and Human Sciences</option>
+                            <option value="College of Humanities and the Arts">College of Humanities and the Arts</option>
+                            <option value="College of Professional and Global Education">College of Professional and Global Education</option>
+                            <option value="College of Science">College of Science</option>
+                            <option value="College of Social Sciences">College of Social Sciences</option> 
+                            <option value="Connie L. Lurie College of Education">Connie L. Lurie College of Education</option>
+                            <option value="Lucas College and Graduate School of Business">Lucas College and Graduate School of Business</option>
+                            <option value="Undeclared">Undeclared</option>
+                            <option value="Undergraduate Education">Undergraduate Education</option>
+                            <option value="Other">Other</option>
+                        </select>
 
-            <div className="form-group">
-            <textarea placeholder="About Me" name="bio" value={bio} onChange={e => onChange(e)}></textarea>
-            <small className="form-text">Tell us a little about yourself and your hobbies</small>
-            </div>
+                    
+                        <p className="inputLabel"> Major </p>
+                        <input type="text" className="createProfileInput" placeholder="Degree(s) & Concentration(s)" name="degrees" value={degrees} onChange={e => onChange(e)}/>
 
-            <div className="my-2">
-            <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
-                Add Social Network Links
-            </button>
-            <span>Optional</span>
-            </div>
 
-            {displaySocialInputs && <Fragment>
-                <div className="form-group social-input">
-                <input type="text" placeholder="Discord Username" name="discord" value={discord} onChange={e => onChange(e)}/>
-                </div>
+                        <p className="inputLabel"> Tell us a bit about yourself </p>
+                        <textarea className="createProfileInput" placeholder="About Me" name="bio" value={bio} onChange={e => onChange(e)}></textarea>
 
-                <div className="form-group social-input">
-                <i className="fab fa-facebook fa-2x"></i>
-                <input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={e => onChange(e)}/>
-                </div>
+                        <div className="my-2">
+                        <button onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
+                            Add Social Network Links
+                        </button>
+                         </div>
+                    </div>
 
-                <div className="form-group social-input">
-                <i className="fab fa-linkedin fa-2x"></i>
-                <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin} onChange={e => onChange(e)}/>
-                </div>
-            </Fragment>}
+                    {displaySocialInputs && <Fragment>
+                        <div className="form-group social-input">
+                        <input type="text" placeholder="Discord Username" name="discord" value={discord} onChange={e => onChange(e)}/>
+                        </div>
 
-            <input type="submit" className="btn btn-primary my-1" />
-            <Link to="/dashboard" className="btn btn-light my-1">Go Back</Link>
-        </form>
-        </Fragment>
+                        <div className="form-group social-input">
+                        <i className="fab fa-facebook fa-2x"></i>
+                        <input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={e => onChange(e)}/>
+                        </div>
+
+                        <div className="form-group social-input">
+                        <i className="fab fa-linkedin fa-2x"></i>
+                        <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin} onChange={e => onChange(e)}/>
+                        </div>
+                    </Fragment>}
+
+                    <input type="submit" className="createProfileButton" />
+                </form>
+        </div>
+    </div>
     )
 }
 
